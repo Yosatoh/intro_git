61322444057fefc971c0ade4bf2cccc0d58a5c4f
github yosatoh <haikigeki@yahoo.co.jp>
2020-05-04 16:17:53 +0900

    initial txt

---
+++ python_command.py
@@ -0,0 +1,7 @@
+# ndaary関数の数値への文字の代入方法
+ y = np.array([0,1,0,1,1]
+ y_named = np.array(['mozi0' 'mozi1'])[y]
+	そのた例：
+	from sklearn.datasets import make_circles
+	X, y = make_circles(noise=0.25, factor = 0.5, random_state=1)
+		#わかりやすいように,クラスを’blue' と'red'にするcfrom sklearn.da		y_named = np.array(['blue', 'red'])[y]
